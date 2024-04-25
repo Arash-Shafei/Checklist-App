2ac4556edb56488d8b93489b0a55e126363e4937
[ArashShafei] <arash_shafei_work@outlook.com>
2024-04-25 10:03:00 -0400

    Fixed the "Back" button Checklist.jsx

--- frontend/src/screens/Checklist.jsx
+++ frontend/src/screens/Checklist.jsx
@@ -1,14 +1,13 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 import { Button } from "@mui/material";
 
 function Checklist() {
   let { name } = useParams();
+  let navigate = useNavigate();
   return (
     <>
-      <Link to={`/group/${name}`}>
-        <Button>Go Back</Button>
-      </Link>
+      <Button onClick={() => navigate(-1)}>Go Back</Button>
       <h1>You will see {name} here</h1>
     </>
   );
